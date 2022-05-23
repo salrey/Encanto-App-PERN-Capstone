6628de6c0612cebea4e7f8d93d5057c83ee8eb23
salrey <tekdehn@gmail.com>
2022-05-23 08:43:43 -0400

    check for currentUser requests, to and from

--- front-end/src/Components/UserDetails.js
+++ front-end/src/Components/UserDetails.js
@@ -9,13 +9,19 @@ const UserDetails = () => {
     const navigate = useNavigate()
     const { id } = useParams();
     const [user, setUser] = useState([]);
+    const [currentUserRequest, setCurrentUserRequest] = useState({
+        request_to: "",
+        request_from: ""
+    });
     const API = process.env.REACT_APP_API_URL;
+
+    console.log([].name)
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const res = await axios.get(`${API}/users/${id}`)
-                res.data.payload.email && setUser(res.data.payload)
+                setUser(res.data.payload)
             } catch (err) {
                 console.warn(err)
                 return navigate("/not-found")
@@ -24,37 +30,61 @@ const UserDetails = () => {
         fetchData();    
     }, [API, id, navigate])
 
-    const handleSwipe = async (event) => {
+    const handleSwipe = (event) => {
         event.preventDefault();
         const submitted = event.nativeEvent.submitter.innerText
-        if (submitted === "No") {
+        const requestToCurrentUser = async () => {
             try {
-                //check if any requests exist for this currentUser
-                const match = await axios.get(`${API}/match-requests?request_to=${currentUser.id}&request_from=${user.id}`)
-                //if so, then delete that request
-                if (match.data.payload.request_to) {
-                    axios.delete(`${API}/match-requests?id=${match.data.payload.id}`)
-                }
-            } catch (err) {
-                //if there are no requests, then navigate to next user
-                return err
-            }
-        } else {
-            try {
-                //check if any requests exist for this currentUser
-                const match = await axios.get(`${API}/match-requests?request_to=${currentUser.id}&request_from=${user.id}`)
-                if (match.data.payload.request_to) {
-                    axios.put(`${API}/match-requests`, {match: {...match.data.payload, request_status: 1}, match_id: match.data.payload.id})
-                    window.alert("Delighted to meet! Let's eat")
-                    //then navigate to chat 
-                } 
-            } catch (err) {
-                console.warn(err)
-                //if no existing request, then create a new one
-                axios.post(`${API}/match-requests`, {request_from: currentUser.id, request_to: user.id})
-                //then navigate to next user
+                const res = await axios.get(`${API}/match-requests?request_to=${currentUser.id}&request_from=${user.id}`)
+                setCurrentUserRequest({...currentUserRequest, request_to: res.data.payload})
+            } catch (error) {
+                return error
             }
         }
+        requestToCurrentUser();
+
+        const requestFromCurrentUser = async () => {
+            try {
+                const res = await axios.get(`${API}/match-requests?request_to=${user.id}&request_from=${currentUser.id}`)
+                setCurrentUserRequest({...currentUserRequest, request_from: res.data.payload})
+            } catch (error) {
+                return error
+            }
+        }
+        requestFromCurrentUser();
+    
+        console.log("Any matches?", currentUserRequest)
+        //need to also consider that if this currentUser also sent that user a request, then changes their mind, so when they say no after saying yes, we are deleting that request 
+        // console.log(fetchData())
+        //if response.data.success is false, then no match_requests exist 
+        // if (submitted === "No") {
+        //     try {
+        //         //check if any requests exist for this currentUser
+        //         const match = await axios.get(`${API}/match-requests?request_to=${currentUser.id}&request_from=${user.id}`)
+        //         //if so, then delete that request
+        //         if (match.data.payload.request_to) {
+        //             axios.delete(`${API}/match-requests?id=${match.data.payload.id}`)
+        //         }
+        //     } catch (err) {
+        //         //if there are no requests, then navigate to next user
+        //         return err
+        //     }
+        // } else {
+        //     try {
+        //         //check if any requests exist for this currentUser
+        //         const match = await axios.get(`${API}/match-requests?request_to=${currentUser.id}&request_from=${user.id}`)
+        //         if (match.data.payload.request_to) {
+        //             axios.put(`${API}/match-requests`, {match: {...match.data.payload, request_status: 1}, match_id: match.data.payload.id})
+        //             window.alert("Delighted to meet! Let's eat")
+        //             //then navigate to chat 
+        //         } 
+        //     } catch (err) {
+        //         console.warn(err)
+        //         //if no existing request, then create a new one
+        //         axios.post(`${API}/match-requests`, {request_from: currentUser.id, request_to: user.id})
+        //         //then navigate to next user
+        //     }
+        // }
     }
     
     return (
