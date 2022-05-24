0c8cac814be2b07b4dcf62314e74733f09374139
kisublee <yigi2021@gmail.com>
2022-05-24 00:06:48 -0400

    Complete basic styling for the login page.

--- front-end/src/Components/UserLogIn.js
+++ front-end/src/Components/UserLogIn.js
@@ -1,6 +1,13 @@
+import { Box, Container, Typography } from "@mui/material";
 import axios from "axios";
 import React, { useState} from "react"
-import {useNavigate } from "react-router-dom"
+import {Link, useNavigate } from "react-router-dom"
+
+//Import images from assets
+import Back from '../Assets/Back.png';
+import UpperBurger from "../Assets/upperHamburger.png"
+import Cheese from "../Assets/Cheese.png"
+import BottomHamburger from "../Assets/Bottom_Hamburger.png"
 
 export default function UserLogIn ({setIsLoggedIn, setCurrentUser}) {
 
@@ -25,12 +32,13 @@ const handleChange = (event) => {
 const handleSubmit = async (event) => {
     
     event.preventDefault();
-
+    // {password:input.password, email:input.email,withCredentials: true}
+    // before there was just input
     const fetchData = async () => {
         try {
           console.log("Hitting logIn page");
-          const res = await axios.post(`${API}/login`, input);
-          setCurrentUser(res.data);
+          const res = await axios.post(`${API}/login`, {password:input.password, email:input.email,withCredentials: true});
+          setCurrentUser(res.data.user);
           setIsLoggedIn(true);
           navigate('/users');
           
@@ -42,27 +50,154 @@ const handleSubmit = async (event) => {
 };
 
     return (
-        <div>
+        <div style={{backgroundColor:"#FFA756"}}>
+            <Container maxWidth="xs">
+            <Link to={'/'}>
+                <img 
+                src={Back} 
+                alt="back"  
+                style={{
+                width: '55px',
+                height: '35px',
+                marginTop:"1.5vh",
+                marginLeft:"1.5vh"
+                }}
+                />
+            </Link>
+            <Typography  
+              sx={{
+              color:"yellow",
+              fontFamily: "Signika Negative",
+              fontSize: "9.5vh",
+              letterSpacing: 1,
+              fontWeight: 900,
+              textAlign:"left",
+              textShadow: "3px 0 yellow",
+              ml:"5vh",
+              mt:"5vh"
+              }}>
+                Hello !
+            </Typography>
+            <Typography
+                sx={{
+                color:"white",
+                fontFamily: "Signika Negative",
+                fontSize: "3.7vh",
+                letterSpacing: 1,
+                fontWeight: 600,
+                textShadow: "2px 0 white",
+                textAlign:"left",
+                ml:"3.1vh",
+                }}>
+                What are you craving?
+            </Typography>
+            <Box 
+               sx={{
+                   display:'flex',
+                   justifyContent:"center",
+                   mt:"5vh"
+               }}
+            >
+            <img src={UpperBurger}/>
+            </Box>
+                <Box sx={{
+                    display:"flex",
+                    textAlign:"center"
+                }}>
             <form onSubmit={handleSubmit}>
                 <input
                     id="email"
                     value={input.email}
                     type="text"
                     onChange={handleChange}
-                    placeholder="email"
+                    placeholder="Email here..."
                     required
+                    style={{
+                        marginTop:"1.5vh",
+                        width:"40vh",
+                        height:"7vh",
+                        borderRadius:"50px",
+                        border:"none",
+                        textAlign:"center",
+                        fontSize:"3vh",
+                       
+                    }}
                     />
+                <img src={Cheese} 
+                    style={{marginTop:"1.5vh"}}
+                />
                      <input
                     id="password"
                     value={input.password}
                     type="text"
                     onChange={handleChange}
-                    placeholder="password"
+                    placeholder="password here..."
                     required
+                    style={{
+                        marginTop:"1.2vh",
+                        height:"7vh",
+                        width:"40vh",
+                        borderRadius:"50px",
+                        border:"none",
+                        textAlign:"center",
+                        fontSize:"3vh",
+                 
+                    }}
                     />
-                    <button>Log In</button>
-            </form>
-        </div>
+                <img src={BottomHamburger} 
+                 style={{
+                     marginTop:"1.5vh"
+                }}
+                />
+                    <button
+                        style={{
+                            borderRadius:"50px",
+                            border:"none",
+                            width:"35vh",
+                            height:"7vh",
+                            marginTop:5,
+                            backgroundColor:"#FF2929",
+                            color:"white",
+                            fontFamily: "Signika Negative",
+                            fontSize: "3.3vh",
+                            letterSpacing: 1,
+                            fontWeight: 600,
+                            textShadow: "2px 0 white",
+                        }}
+                    >Log In
+                    </button>
+                    <Typography 
+                    sx={{color:"white",
+                        fontWeight: 600,
+                        mt:"2vh",
+                        fontSize: "1.8vh",
+                        fontFamily: "Signika Negative",
+                    }}
+                    >
+                        Forgot your password? click 
+                       <Link to={'/'}>
+                        <span style={{color:"#4A76B8", textDecoration:"underline"}}> 
+                        here 
+                        </span> 
+                       </Link>
+                    </Typography>
+                    <Typography
+                        sx={{
+                        color:"white",
+                        fontFamily: "Signika Negative",
+                        fontSize: "1.8vh",
+                        letterSpacing: 2,
+                        fontWeight: 600,
+                        textAlign:"center",
+                        mt:"15.2vh"
+                        }}
+                    >
+                        EnCanto 2022 Terms
+                    </Typography>
+                </form>
+            </Box>
+        </Container>
+    </div>
     )
 
 }
