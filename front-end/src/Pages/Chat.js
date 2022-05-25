d1709f5659115141ff9b1389a5fd701f28ea9401
salrey <tekdehn@gmail.com>
2022-05-25 09:40:07 -0400

    refactor, add message for accepted matches, alert if no users were found

--- front-end/src/Pages/Chat.js
+++ front-end/src/Pages/Chat.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Footer from '../Components/Footer';
 
 const Chat = () => {
   return (
-    <div> Chat in progress</div>
+    <>
+      <div> Chat in progress</div>
+      <Footer/>
+    </>
   );
 }
 
