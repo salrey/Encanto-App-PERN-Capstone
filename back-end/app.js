1a7fdc57251aab4cc95dceee8694292a58be1fcf
salrey <tekdehn@gmail.com>
2022-06-01 06:15:17 -0400

    refactor, add multer dependency

--- back-end/app.js
+++ back-end/app.js
@@ -4,34 +4,34 @@ const express = require("express");
 const flash = require("express-flash");
 const session = require('express-session');
 const passport = require("passport");
-// const methodOverride = require("method-override")
+const initializePassport = require('./passport-config');
 
 const userController = require("./controllers/userController");
 const matchController = require("./controllers/matchController");
 const { getOneUserByEmail, getOneUser } = require('./queries/users')
+
 
 // CONFIGURATION
 const app = express();
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-const initializePassport = require('./passport-config');
 initializePassport(passport, getOneUserByEmail, getOneUser);
 
+
 // MIDDLEWARE
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(cors());
 app.use(flash())
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
-  cookie: { secure: true }
 }))
 app.use(passport.initialize());
 app.use(passport.session());
-// app.use(methodOverride('_method'))
+
 
 // ROUTES
 app.use("/users", userController);
@@ -39,9 +39,6 @@ app.use("/match-requests", matchController);
 
 app.get("/", (_, res) => {
   res.status(200).send("Welcome to EnCanto!");
-});
-app.get("*", (_, res) => {
-  res.status(404).send("This page has not been found");
 });
 
 app.post('/login', (req, res, next) => {
@@ -52,7 +49,6 @@ app.post('/login', (req, res, next) => {
     }
     if (!currentUser) {
       return res.status(401).json(info);
-      // return;
     }
     console.log("failure details from line 76: ", info)
     // save user in session
@@ -68,12 +64,9 @@ app.post('/login', (req, res, next) => {
   })(req, res, next)
 })
 
-//Logout in progress
-// app.delete("/logout", (req, res) => {
-//   req.logOut();
-//   res.redirect('/login')
-// })
-
+app.get("*", (_, res) => {
+  res.status(404).send("This page has not been found");
+});
 
 // EXPORT
 module.exports = app;
